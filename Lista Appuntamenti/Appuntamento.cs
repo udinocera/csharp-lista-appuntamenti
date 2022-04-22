b0f953a1082e92e428a49b8975e58a7ea2c89312
udinocera <udinocera@gmail.com>
2022-04-22 20:19:49 +0200

    Esercizio

---
+++ Lista Appuntamenti/Appuntamento.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAppuntamenti
+{
+    internal class Appuntamento 
+    {
+        public DateTime dataAppuntamento;
+        public string nome;
+        public string localitaAppuntamento;
+        public bool appuntamentoValido = true;
+
+        public Appuntamento(DateTime dataAppuntamento, string nome, string localitaAppuntamento)
+
+            //TRY CATCH 
+        {
+            
+            try
+            {
+                verificaDataAppuntamento(dataAppuntamento);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                appuntamentoValido = false;
+            }
+
+            this.nome = nome;
+            this.localitaAppuntamento = localitaAppuntamento;
+        }
+
+        //metodo 
+        public void verificaDataAppuntamento(DateTime dataAppuntamento)
+        {
+            if (dataAppuntamento < DateTime.Now)
+            {
+                throw new Exception("La data inserita è nel passato");
+            }
+            else
+            {
+                this.dataAppuntamento = dataAppuntamento;
+            }
+        }
+
+        public string GetNome()
+        { return nome; }
+
+        public DateTime GetDataAppuntamento()
+        { return dataAppuntamento; }
+
+        //metodo 
+        public DateTime cambiaData(DateTime nuovaData)
+        {
+            appuntamentoValido = true;
+            try
+            {
+                verificaDataAppuntamento(nuovaData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                appuntamentoValido = false;
+            }
+            return dataAppuntamento;
+        }
+
+        public override string ToString()
+        {
+            string stampaAppuntamento = "Prova";
+
+            stampaAppuntamento += "\n-------Appuntamento------- \n";
+            stampaAppuntamento += "Nome: " + nome + "\n";
+            stampaAppuntamento += "Data: " + dataAppuntamento + "\n";
+            stampaAppuntamento += "Località: " + localitaAppuntamento + "\n";
+            stampaAppuntamento += "--------------------------";
+            return stampaAppuntamento;
+        }
+
+    }
+}
--- Lista Appuntamenti/Program.cs
+++ Lista Appuntamenti/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ListaAppuntamenti;
+
+Console.WriteLine("AGENDA \n");
+
+List<Appuntamento> listaAppuntamenti = new List<Appuntamento>();
+
+Console.Write("Quanti appuntamenti hai? ");
+int numeroAppuntamenti = Convert.ToInt32(Console.ReadLine());
+
+DateTime dataDaAggiungere;
+string nomeAppuntamento;
+string localita;
+
+while (numeroAppuntamenti > 0)
+{
+    Console.WriteLine("\n-------Appuntamento-------");
+    Console.Write("Inserisci il nome : ");
+    nomeAppuntamento = Console.ReadLine();
+    Console.Write("Inserisci la data : ");
+    dataDaAggiungere = DateTime.Parse(Console.ReadLine());
+    Console.Write("Inserisci la località : ");
+    localita = Console.ReadLine();
+    Console.WriteLine("--------------------------");
+
+
+    listaAppuntamenti.Add(new Appuntamento(dataDaAggiungere, nomeAppuntamento, localita));
+    if (listaAppuntamenti[listaAppuntamenti.Count - 1].appuntamentoValido)
+    {
+        numeroAppuntamenti--;
+    }
+    else
+    {
+        listaAppuntamenti.RemoveAt(listaAppuntamenti.Count - 1);
+    }
+}
+
+
